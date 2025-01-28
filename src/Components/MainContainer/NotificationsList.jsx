2e30bc74bc9639e5b03871e146110d40e7b9376e
./trenches <romanvagner@mail.com>
2025-01-28 16:37:05 +0100

    Added notifications list files

--- src/Components/MainContainer/NotificationsList.jsx
+++ src/Components/MainContainer/NotificationsList.jsx
@@ -1,6 +1,37 @@
+import './NotificationsList.css';
+
 const NotificationsList = () => {
+
+  const notifications = [
+    {
+      name: 'Chris Travis',
+      action: 'Added your contact',
+      date: '2 hours ago',
+    },
+    {
+      name: 'John Bow',
+      action: 'Missed Call',
+      date: '1 day ago',
+    },
+    {
+      name: 'Peter Braski',
+      action: 'Mentioned you in the group "Gym bros"',
+      date: '2 days ago',
+    },
+]
+
   return (
-    <div>Notifications</div>
+    <div className="notifications-list">
+      {notifications.map((notification) => (
+        <>
+          <div className="notification-item">
+            <h3>{notification.name}</h3>
+            <p>{notification.action}</p>
+            <p>{notification.date}</p>
+          </div>
+        </>
+      ))}
+    </div>
   )
 }
 
