433a5dff7793d492b4a560260f23aba9152b5183
./trenches <romanvagner@mail.com>
2025-01-29 19:57:32 +0100

    Added settings container + change styles for CallsList and NotificationList

--- src/Components/MainContainer/Settings.jsx
+++ src/Components/MainContainer/Settings.jsx
@@ -1,6 +1,21 @@
+import './Settings.css';
+
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className="settings-container">
+      <div className="settings-profile">
+        <img src="https://hype5-react.vercel.app/images/avatars/Vector-10.png" alt="profile picture" />
+        <p className='username'>Chatter User</p>
+        <h3>Information</h3>
+        <h4>Name: Chatter User</h4>
+        <h4>Email: example@example.com</h4>
+        <h4>Phone: +1 234 23456789</h4>
+        <h4>Address: Chicago, USA</h4>
+      </div>
+      <div className="theme-toggle">
+        <button className='switchtheme-btn'>Switch theme</button>
+      </div>
+    </div>
   )
 }
 
